2b3777cd8bf0652f6e67c98ce7547af878130bc3
onevcat <onevcat@gmail.com>
2019-05-17 16:48:14 +0900

    Application and basic app delegate

--- ios/Classes/SwiftFlutterLineSdkPlugin.swift
+++ ios/Classes/SwiftFlutterLineSdkPlugin.swift
@@ -8,9 +8,26 @@ public class SwiftFlutterLineSdkPlugin: NSObject, FlutterPlugin {
     let channel = FlutterMethodChannel(name: "com.linecorp/flutter_line_sdk", binaryMessenger: registrar.messenger())
     let instance = SwiftFlutterLineSdkPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
+    registrar.addApplicationDelegate(instance)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     result("LINE SDK Version " + LineSDK.Constant.SDKVersion)
+  }
+
+  public func application(
+    _ application: UIApplication,
+    open url: URL,
+    options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool
+  {
+    return LoginManager.shared.application(application, open: url, options: options)
+  }
+
+  public func application(
+    _ application: UIApplication,
+    continue userActivity: NSUserActivity,
+    restorationHandler: @escaping ([Any]) -> Void) -> Bool
+  {
+    return LoginManager.shared.application(application, open: userActivity.webpageURL)
   }
 }
--- lib/src/line_sdk.dart
+++ lib/src/line_sdk.dart
@@ -3,9 +3,31 @@ part of flutter_line_sdk;
 class LineSDK {
   static const MethodChannel _channel =
       const MethodChannel('com.linecorp/flutter_line_sdk');
+  
+  static final LineSDK instance = LineSDK._();
+
+  LineSDK._() {
+    _channel.setMethodCallHandler(_callHandler);
+  }
+
+  Future<void> setup(String channelID, String universalLink) async {
+    _channel.invokeMethod(
+      'setup',
+      <String, String>{
+        'channelID': channelID, 
+        'universalLink': universalLink
+      }
+    );
+  }
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
+  }
+
+  Future<void> _callHandler(MethodCall call) async {
+    switch (call.method) {
+
+    }
   }
 }
