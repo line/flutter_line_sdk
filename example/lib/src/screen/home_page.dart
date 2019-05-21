ad2956a03efdab41f38c1e30042c3068832cc1c6
Daniel Kao <daniel.kao@linecorp.com>
2019-05-21 18:44:49 +0800

    reformat code for home_page.dart

--- example/lib/src/screen/home_page.dart
+++ example/lib/src/screen/home_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_line_sdk/flutter_line_sdk.dart';
@@ -11,8 +10,8 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> with AutomaticKeepAliveClientMixin {
-  
+class _HomePageState extends State<HomePage>
+    with AutomaticKeepAliveClientMixin {
   UserProfile _userProfile;
   String _accessToken;
   bool _isOnlyWebLogin = false;
@@ -30,7 +29,6 @@ class _HomePageState extends State<HomePage> with AutomaticKeepAliveClientMixin 
 
   // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> initPlatformState() async {
-    
     UserProfile userProfile;
     String accessToken;
 
@@ -65,78 +63,77 @@ class _HomePageState extends State<HomePage> with AutomaticKeepAliveClientMixin 
         child: Column(
           children: <Widget>[
             Card(
-              child: Column (
+              child: Column(children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(15.0),
+                  child: Text(
+                    "Configurations",
+                    style: Theme.of(context).textTheme.title,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 15.0),
+                  child: _scopeListUI(),
+                ),
+                Row(
                   children: <Widget>[
-                    Padding(
-                      padding: EdgeInsets.all(15.0),
-                      child: Text("Configurations",
-                        style: Theme.of(context).textTheme.title,),
+                    Checkbox(
+                      activeColor: accentColor,
+                      value: _isOnlyWebLogin,
+                      onChanged: (bool value) {
+                        setState(() {
+                          _isOnlyWebLogin = !_isOnlyWebLogin;
+                        });
+                      },
                     ),
-                    Padding(
-                      padding: EdgeInsets.only(left: 15.0),
-                      child: _scopeListUI(),
-                    ),
-                    Row(
-                      children: <Widget>[
-                        Checkbox(
-                          activeColor: accentColor,
-                          value: _isOnlyWebLogin,
-                          onChanged: (bool value) {
-                            setState(() {
-                              _isOnlyWebLogin = !_isOnlyWebLogin;
-                            });
-                          },
-                        ),
-                        Text("only Web Login"),
-                      ],
-                    )
-                  ]
-              ),
+                    Text("only Web Login"),
+                  ],
+                )
+              ]),
             ),
             Expanded(
                 child: Center(
-                  child: RaisedButton(
-                    textColor: textColor,
-                    color: accentColor,
-                    onPressed: _signIn,
-                    child: Text('Sign In'),
-                  ),
-                )
-            ),
+              child: RaisedButton(
+                textColor: textColor,
+                color: accentColor,
+                onPressed: _signIn,
+                child: Text('Sign In'),
+              ),
+            )),
           ],
         ),
       );
     } else {
       return UserInfoWidget(
-        userProfile: _userProfile, 
-        onSignOutPressed: _signOut);
+          userProfile: _userProfile, onSignOutPressed: _signOut);
     }
   }
 
   Widget _scopeListUI() {
     var widgetList = <Widget>[(Text("Scopes: "))];
-    widgetList.addAll(
-        _scopes.map<Widget>((String scope) {
-          return Padding(
-            padding: const EdgeInsets.only(left: 4.0, right: 4.0),
-            child: ChipTheme(
-              data: ChipTheme.of(context).copyWith(brightness: Brightness.dark),
-              child: FilterChip(
-                label: Text(scope, style: TextStyle(color: textColor),),
-                selectedColor: accentColor,
-                selected: _selectedScopes.contains(scope),
-                onSelected: (bool value) {
-                  setState(() {
-                    (!_selectedScopes.contains(scope)) ?
-                    _selectedScopes.add(scope) :
-                    _selectedScopes.remove(scope);
-                  });
-                },
-              ),
+    widgetList.addAll(_scopes.map<Widget>((String scope) {
+      return Padding(
+        padding: const EdgeInsets.only(left: 4.0, right: 4.0),
+        child: ChipTheme(
+          data: ChipTheme.of(context).copyWith(brightness: Brightness.dark),
+          child: FilterChip(
+            label: Text(
+              scope,
+              style: TextStyle(color: textColor),
             ),
-          );
-        }).toList()
-    );
+            selectedColor: accentColor,
+            selected: _selectedScopes.contains(scope),
+            onSelected: (bool value) {
+              setState(() {
+                (!_selectedScopes.contains(scope))
+                    ? _selectedScopes.add(scope)
+                    : _selectedScopes.remove(scope);
+              });
+            },
+          ),
+        ),
+      );
+    }).toList());
 
     return Row(children: widgetList);
   }
@@ -144,16 +141,15 @@ class _HomePageState extends State<HomePage> with AutomaticKeepAliveClientMixin 
   void _signIn() async {
     try {
       final result = await LineSDK.instance.login(
-        scopes: _selectedScopes.toList(),
-        option: LoginOption(_isOnlyWebLogin, "normal")
-      );
+          scopes: _selectedScopes.toList(),
+          option: LoginOption(_isOnlyWebLogin, "normal"));
 
       setState(() {
         _userProfile = result.userProfile;
         _accessToken = result.accessToken.value;
       });
     } on PlatformException catch (e) {
-        _showDialog(context, e.toString());
+      _showDialog(context, e.toString());
     }
   }
 
@@ -164,19 +160,17 @@ class _HomePageState extends State<HomePage> with AutomaticKeepAliveClientMixin 
         _userProfile = null;
         _accessToken = null;
       });
-    } on PlatformException catch (e) {
-      
-    }
+    } on PlatformException catch (e) {}
   }
 
   void _showDialog(BuildContext context, String text) {
-    showDialog(context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          content: Text(text),
-        );
-      }
-    );
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            content: Text(text),
+          );
+        });
   }
 }
 
