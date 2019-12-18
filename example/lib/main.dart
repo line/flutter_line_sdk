250d49113170ed0a82cfefd766319706b1ed90b6
onevcat <onevcat@gmail.com>
2019-12-18 16:31:57 +0900

    Fix crashing for example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter_line_sdk/flutter_line_sdk.dart';
 import 'src/app.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   LineSDK.instance.setup("1620019587").then((_) {
     print("LineSDK Prepared");
   });
